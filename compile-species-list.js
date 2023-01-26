61e872e7770009fcf896415f31ae1f871ac8acb3
rafnuss <rafnuss@gmail.com>
2023-01-26 11:14:41 -0500

    update species list

--- compile-species-list.js
+++ compile-species-list.js
@@ -7,7 +7,7 @@ console.log(full_species_list)
 let species_list = {};
 
 full_species_list.forEach(s => {
-    species_list[s.id] = s.SPECIES_CODE;
+    species_list[s.id] = s.ebird_species_code;
 })
 
 fs.writeFileSync("./data/biolovision_species_list_short.json", JSON.stringify(species_list));
